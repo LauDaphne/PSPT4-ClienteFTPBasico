794065206d0cfda13828f3a0297d05da23957eb4
LauDaphne <laura.mp.job@gmail.com>
2020-02-26 15:12:57 +0100

    Vista del cliente realizado

--- PSPT4-ClienteFTPBasico/src/es/studium/clienteFTPBasico/ClienteFTPBasico.java
+++ PSPT4-ClienteFTPBasico/src/es/studium/clienteFTPBasico/ClienteFTPBasico.java
@@ -3,7 +3,6 @@ package es.studium.clienteFTPBasico;
 import java.awt.Color;
 import java.awt.Container;
 import java.awt.Dimension;
-import java.awt.FlowLayout;
 import java.awt.Font;
 import java.awt.Rectangle;
 import java.awt.event.ActionEvent;
@@ -32,17 +31,22 @@ import org.apache.commons.net.PrintCommandListener;
 import org.apache.commons.net.ftp.FTP;
 import org.apache.commons.net.ftp.FTPClient;
 import org.apache.commons.net.ftp.FTPFile;
+import javax.swing.JLabel;
+import javax.swing.JSeparator;
+import javax.swing.SwingConstants;
+import javax.swing.border.LineBorder;
+import java.awt.SystemColor;
 
 public class ClienteFTPBasico extends JFrame 
 {
 	private static final long serialVersionUID = 1L;
 	// Campos de la cabecera parte superior
-	static JTextField txtServidor = new JTextField();
-	static JTextField txtUsuario = new JTextField();
-	static JTextField txtDirectorioRaiz = new JTextField();
+	static JLabel txtServidor = new JLabel();
+	static JLabel txtUsuario = new JLabel();
+	static JLabel txtDirectorioRaiz = new JLabel();
 	// Campos de mensajes parte inferior
-	private static JTextField txtArbolDirectoriosConstruido = new JTextField();
-	private static JTextField txtActualizarArbol = new JTextField();
+	private static JLabel txtArbolDirectoriosConstruido = new JLabel();
+	private static JLabel txtActualizarArbol = new JLabel();
 	// Botones
 	JButton botonCargar = new JButton("Subir fichero");
 	JButton botonDescargar = new JButton("Descargar fichero");
@@ -57,13 +61,22 @@ public class ClienteFTPBasico extends JFrame
 	// Datos del servidor FTP - Servidor local
 	static FTPClient cliente = new FTPClient();// cliente FTP
 	String servidor = "127.0.0.1";
-	String user = "jorge";
+	String user = "Laura";
 	String pasw = "Studium2019;";
 	boolean login;
 	static String direcInicial = "/";
 	// para saber el directorio y fichero seleccionado
 	static String direcSelec = direcInicial;
 	static String ficheroSelec = "";
+	private final JLabel lblCarpetas = new JLabel("CARPETAS");
+	private final JLabel lblFicheros = new JLabel("FICHEROS");
+	private JTextField txtNombreCarpetaNuevo;
+	private JTextField txtNombreFicheroNuevo;
+	private final JLabel lblDatos = new JLabel("DATOS");
+	private final JSeparator separator_1 = new JSeparator();
+	private final JSeparator separator_2 = new JSeparator();
+	private final JSeparator separator_3 = new JSeparator();
+	private final JSeparator separator_4 = new JSeparator();
 	public static void main(String[] args) throws IOException 
 	{
 		new ClienteFTPBasico();
@@ -72,6 +85,9 @@ public class ClienteFTPBasico extends JFrame
 	public ClienteFTPBasico() throws IOException
 	{
 		super("CLIENTE BÁSICO FTP");
+		getContentPane().setBackground(SystemColor.inactiveCaption);
+		getContentPane().setForeground(SystemColor.desktop);
+		setResizable(false);
 		//para ver los comandos que se originan
 		cliente.addProtocolCommandListener(new PrintCommandListener(new PrintWriter (System.out)));
 		cliente.connect(servidor); //conexión al servidor
@@ -82,13 +98,19 @@ public class ClienteFTPBasico extends JFrame
 		//Obteniendo ficheros y directorios del directorio actual
 		FTPFile[] files = cliente.listFiles();
 		llenarLista(files,direcInicial);
+		txtArbolDirectoriosConstruido.setFont(new Font("Tahoma", Font.BOLD, 11));
+		txtArbolDirectoriosConstruido.setBounds(11, 491, 716, 20);
 		//Construyendo la lista de ficheros y directorios
 		//del directorio de trabajo actual		
 		//preparar campos de pantalla
 		txtArbolDirectoriosConstruido.setText("<< ARBOL DE DIRECTORIOS CONSTRUIDO >>");
+		txtServidor.setBounds(500, 348, 142, 20);
 		txtServidor.setText("Servidor FTP: "+servidor);
+		txtUsuario.setBounds(500, 410, 128, 20);
 		txtUsuario.setText("Usuario: "+user);
+		txtDirectorioRaiz.setBounds(500, 379, 137, 20);
 		txtDirectorioRaiz.setText("DIRECTORIO RAIZ: "+direcInicial);
+		getContentPane().setLayout(null);
 		//Preparación de la lista
 		//se configura el tipo de selección para que solo se pueda
 		//seleccionar un elemento de la lista
@@ -97,24 +119,140 @@ public class ClienteFTPBasico extends JFrame
 		//barra de desplazamiento para la lista
 		JScrollPane barraDesplazamiento = new JScrollPane(listaDirec);
 		barraDesplazamiento.setPreferredSize(new Dimension(335,420));
-		barraDesplazamiento.setBounds(new Rectangle(5,65,335,420));
+		barraDesplazamiento.setBounds(new Rectangle(11, 37, 353, 405));
 		c.add(barraDesplazamiento);
+		botonCargar.setBounds(573, 100, 154, 23);
+		c.add(botonCargar);
+		//final del botón Eliminar Carpeta
+		botonCargar.addActionListener(new ActionListener()
+		{
+			@Override
+			public void actionPerformed(ActionEvent e)
+			{
+				JFileChooser f;
+				File file;
+				f = new JFileChooser();
+				//solo se pueden seleccionar ficheros
+				f.setFileSelectionMode(JFileChooser.FILES_ONLY);
+				//t�tulo de la ventana
+				f.setDialogTitle("Selecciona el fichero a subir al servidor FTP");
+				//se muestra la ventana
+				int returnVal = f.showDialog(f, "Cargar");
+				if (returnVal == JFileChooser.APPROVE_OPTION) 
+				{
+					//fichero seleccionado
+					file = f.getSelectedFile();
+					//nombre completo del fichero
+					String archivo = file.getAbsolutePath();
+					//solo nombre del fichero
+					String nombreArchivo = file.getName();
+					try 
+					{
+						SubirFichero(archivo, nombreArchivo);
+					}
+					catch (IOException e1) 
+					{
+						e1.printStackTrace(); 
+					}
+				}
+			}
+		}); //Fin botón subir
 		c.add(txtServidor);
 		c.add(txtUsuario);
 		c.add(txtDirectorioRaiz);
 		c.add(txtArbolDirectoriosConstruido);
+		txtActualizarArbol.setFont(new Font("Tahoma", Font.BOLD, 11));
+		txtActualizarArbol.setBorder(null);
+		txtActualizarArbol.setBounds(11, 460, 716, 20);
 		c.add(txtActualizarArbol);
-		c.add(botonCargar);
+		botonCreaDir.setBounds(374, 100, 154, 23);
 		c.add(botonCreaDir);
+		botonDelDir.setBounds(374, 134, 154, 23);
 		c.add(botonDelDir);
+		botonDescargar.setBounds(573, 134, 154, 23);
 		c.add(botonDescargar);
+		botonBorrar.setBounds(573, 167, 154, 23);
 		c.add(botonBorrar);
+		botonSalir.setBounds(304, 522, 132, 36);
 		c.add(botonSalir);
 		c.setLayout(null);
+		
+		JLabel lblArbolDirectorios = new JLabel("ÁRBOL DE DIRECTORIOS");
+		lblArbolDirectorios.setFont(new Font("Tahoma", Font.BOLD, 14));
+		lblArbolDirectorios.setBounds(11, 12, 226, 23);
+		getContentPane().add(lblArbolDirectorios);
+		
+		JLabel lblAcciones = new JLabel("ACCIONES");
+		lblAcciones.setFont(new Font("Tahoma", Font.BOLD, 14));
+		lblAcciones.setBounds(516, 12, 78, 23);
+		getContentPane().add(lblAcciones);
+		
+		JSeparator separator = new JSeparator();
+		separator.setOrientation(SwingConstants.VERTICAL);
+		separator.setBackground(Color.BLACK);
+		separator.setBounds(549, 67, 9, 226);
+		getContentPane().add(separator);
+		lblCarpetas.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		lblCarpetas.setBounds(422, 66, 78, 23);
+		
+		getContentPane().add(lblCarpetas);
+		lblFicheros.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		lblFicheros.setBounds(616, 67, 78, 23);
+		
+		getContentPane().add(lblFicheros);
+		
+		JButton btnRenombrarCarpeta = new JButton("Renombrar carpeta");
+		btnRenombrarCarpeta.setBounds(374, 168, 154, 23);
+		getContentPane().add(btnRenombrarCarpeta);
+		
+		txtNombreCarpetaNuevo = new JTextField();
+		txtNombreCarpetaNuevo.setEnabled(false);
+		txtNombreCarpetaNuevo.setBounds(374, 202, 154, 20);
+		getContentPane().add(txtNombreCarpetaNuevo);
+		txtNombreCarpetaNuevo.setColumns(10);
+		
+		JButton btnCambiarCarpeta = new JButton("Cambiar");
+		btnCambiarCarpeta.setEnabled(false);
+		btnCambiarCarpeta.setBounds(405, 233, 95, 23);
+		getContentPane().add(btnCambiarCarpeta);
+		
+		JButton btnRenombrarFichero = new JButton("Renombrar fichero");
+		btnRenombrarFichero.setBounds(573, 200, 154, 23);
+		getContentPane().add(btnRenombrarFichero);
+		
+		txtNombreFicheroNuevo = new JTextField();
+		txtNombreFicheroNuevo.setEnabled(false);
+		txtNombreFicheroNuevo.setColumns(10);
+		txtNombreFicheroNuevo.setBounds(573, 229, 154, 20);
+		getContentPane().add(txtNombreFicheroNuevo);
+		
+		JButton btnCambiarFichero = new JButton("Cambiar");
+		btnCambiarFichero.setEnabled(false);
+		btnCambiarFichero.setBounds(599, 260, 95, 23);
+		getContentPane().add(btnCambiarFichero);
+		lblDatos.setFont(new Font("Tahoma", Font.BOLD, 14));
+		lblDatos.setBounds(526, 314, 78, 23);
+		
+		getContentPane().add(lblDatos);
+		separator_1.setBackground(Color.BLACK);
+		separator_1.setBounds(365, 65, 381, 13);
+		
+		getContentPane().add(separator_1);
+		separator_2.setBackground(Color.BLACK);
+		separator_2.setBounds(364, 291, 381, 13);
+		
+		getContentPane().add(separator_2);
+		separator_3.setBackground(Color.BLACK);
+		separator_3.setBounds(365, 88, 381, 13);
+		
+		getContentPane().add(separator_3);
+		separator_4.setBackground(Color.BLACK);
+		separator_4.setBounds(363, 440, 381, 13);
+		
+		getContentPane().add(separator_4);
 		//se añaden el resto de los campos de pantalla
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setLayout(new FlowLayout());
-		setSize(510,600);
+		setSize(748,604);
 		setVisible(true);
 		//Acciones al pulsar en la lista o en los botones
 		listaDirec.addListSelectionListener(new ListSelectionListener()
@@ -231,40 +369,6 @@ public class ClienteFTPBasico extends JFrame
 				// final del if
 			}
 		}); 
-		//final del botón Eliminar Carpeta
-		botonCargar.addActionListener(new ActionListener()
-		{
-			@Override
-			public void actionPerformed(ActionEvent e)
-			{
-				JFileChooser f;
-				File file;
-				f = new JFileChooser();
-				//solo se pueden seleccionar ficheros
-				f.setFileSelectionMode(JFileChooser.FILES_ONLY);
-				//t�tulo de la ventana
-				f.setDialogTitle("Selecciona el fichero a subir al servidor FTP");
-				//se muestra la ventana
-				int returnVal = f.showDialog(f, "Cargar");
-				if (returnVal == JFileChooser.APPROVE_OPTION) 
-				{
-					//fichero seleccionado
-					file = f.getSelectedFile();
-					//nombre completo del fichero
-					String archivo = file.getAbsolutePath();
-					//solo nombre del fichero
-					String nombreArchivo = file.getName();
-					try 
-					{
-						SubirFichero(archivo, nombreArchivo);
-					}
-					catch (IOException e1) 
-					{
-						e1.printStackTrace(); 
-					}
-				}
-			}
-		}); //Fin botón subir
 		botonDescargar.addActionListener(new ActionListener()
 		{
 			@Override
